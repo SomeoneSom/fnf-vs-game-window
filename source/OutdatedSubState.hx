e4f267d27e0ae443ca4998058f920e755635f456
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2021-05-14 13:13:58 -0700

    init commit

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"PLEASE READ BEFORE PLAYING\n"
+			+ "This mod is best played in windowed mode.\nFullscreen will most likely bug and a maximized window won't do much."
+			+ "If you are recording or streaming this, RECORD YOUR WHOLE MONITOR.\n"
+			+ "Also for the love of all that is holy, don't make your window huge.\n"
+			+ "Press Escape to close this.",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			//FlxG.openURL("https://github.com/KadeDev/Kade-Engine/releases/latest");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
